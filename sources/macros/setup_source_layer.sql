044b815c1b5b3298680b6d22d05e72984dc90655
AntoineGiraud <antoine.giraud@outlook.com>
2026-05-31 09:49:55 +0200

    refactor: compagny dans manifest

--- sources/macros/setup_source_layer.sql
+++ sources/macros/setup_source_layer.sql
@@ -4,8 +4,8 @@
 {% macro setup_source_layer(src_name) %}
 
     -- 1. Propriété : Loader charge le brut, Transformer gère le staging
-    grant ownership on schema BIKESHARE{{env_suffix}}.BRONZE_{{src_name}} to role BIKESHARE_LOADER{{env_suffix}} revoke current grants;
-    grant ownership on schema BIKESHARE{{env_suffix}}.SILVER_{{src_name}} to role BIKESHARE_TRANSFORMER{{env_suffix}} revoke current grants;
+    grant ownership on schema {{compagny}}{{env_suffix}}.BRONZE_{{src_name}} to role {{compagny}}_LOADER{{env_suffix}} revoke current grants;
+    grant ownership on schema {{compagny}}{{env_suffix}}.SILVER_{{src_name}} to role {{compagny}}_TRANSFORMER{{env_suffix}} revoke current grants;
 
     -- 2. Variables Jinja pour boucler proprement
     {% set source_schemas = ['BRONZE_' ~ src_name, 'SILVER_' ~ src_name] %}
@@ -14,11 +14,11 @@
     -- 3. Application des droits de lecture
     {% for sch in source_schemas %}
         -- Accès au conteneur (schéma)
-        grant usage on schema BIKESHARE{{env_suffix}}.{{sch}} to role BIKESHARE_READER{{env_suffix}};
+        grant usage on schema {{compagny}}{{env_suffix}}.{{sch}} to role {{compagny}}_READER{{env_suffix}};
         -- Accès aux objets (Existants et Futurs)
         {% for obj in object_types %}
-            grant select on all {{obj}} in schema BIKESHARE{{env_suffix}}.{{sch}} to role BIKESHARE_READER{{env_suffix}};
-            grant select on future {{obj}} in schema BIKESHARE{{env_suffix}}.{{sch}} to role BIKESHARE_READER{{env_suffix}};
+            grant select on all {{obj}} in schema {{compagny}}{{env_suffix}}.{{sch}} to role {{compagny}}_READER{{env_suffix}};
+            grant select on future {{obj}} in schema {{compagny}}{{env_suffix}}.{{sch}} to role {{compagny}}_READER{{env_suffix}};
         {% endfor %}
     {% endfor %}
 
--- sources/macros/setup_team_layer.sql
+++ sources/macros/setup_team_layer.sql
@@ -4,18 +4,18 @@
 {% macro setup_team_layer(team_name) %}
 
     -- 1. Création du rôle reader de l'équipe
-    define role BIKESHARE_{{team_name}}_READER{{env_suffix}} comment = '🚲 Reads {{team_name}} specific data';
-    grant role BIKESHARE_{{team_name}}_READER{{env_suffix}} to role BIKESHARE_READER{{env_suffix}};
+    define role {{compagny}}_{{team_name}}_READER{{env_suffix}} comment = '🚲 Reads {{team_name}} specific data';
+    grant role {{compagny}}_{{team_name}}_READER{{env_suffix}} to role {{compagny}}_READER{{env_suffix}};
 
     -- 2. Propriété : Manual Inputs et Couches Métier
-    grant ownership on schema BIKESHARE{{env_suffix}}.BRONZE_MI_{{team_name}} to role BIKESHARE_LOADER{{env_suffix}} revoke current grants;
-    grant ownership on schema BIKESHARE{{env_suffix}}.SILVER_MI_{{team_name}} to role BIKESHARE_TRANSFORMER{{env_suffix}} revoke current grants;
-    grant ownership on schema BIKESHARE{{env_suffix}}.SILVER_{{team_name}} to role BIKESHARE_TRANSFORMER{{env_suffix}} revoke current grants;
-    grant ownership on schema BIKESHARE{{env_suffix}}.GOLD_{{team_name}} to role BIKESHARE_TRANSFORMER{{env_suffix}} revoke current grants;
+    grant ownership on schema {{compagny}}{{env_suffix}}.BRONZE_MI_{{team_name}} to role {{compagny}}_LOADER{{env_suffix}} revoke current grants;
+    grant ownership on schema {{compagny}}{{env_suffix}}.SILVER_MI_{{team_name}} to role {{compagny}}_TRANSFORMER{{env_suffix}} revoke current grants;
+    grant ownership on schema {{compagny}}{{env_suffix}}.SILVER_{{team_name}} to role {{compagny}}_TRANSFORMER{{env_suffix}} revoke current grants;
+    grant ownership on schema {{compagny}}{{env_suffix}}.GOLD_{{team_name}} to role {{compagny}}_TRANSFORMER{{env_suffix}} revoke current grants;
 
     -- 3. Accès de base pour le rôle de l'équipe
-    grant usage on database BIKESHARE{{env_suffix}} to role BIKESHARE_{{team_name}}_READER{{env_suffix}};
-    grant usage on warehouse BIKESHARE_READING_WH{{env_suffix}} to role BIKESHARE_{{team_name}}_READER{{env_suffix}};
+    grant usage on database {{compagny}}{{env_suffix}} to role {{compagny}}_{{team_name}}_READER{{env_suffix}};
+    grant usage on warehouse {{compagny}}_READING_WH{{env_suffix}} to role {{compagny}}_{{team_name}}_READER{{env_suffix}};
 
     -- 4. Droits de lecture sur les schémas de l'équipe
     {% set team_schemas = ['BRONZE_MI_' ~ team_name, 'SILVER_MI_' ~ team_name, 'SILVER_' ~ team_name, 'GOLD_' ~ team_name] %}
@@ -23,12 +23,12 @@
 
     {% for sch in team_schemas %}
         -- Accès au conteneur (schéma)
-        grant usage on schema BIKESHARE{{env_suffix}}.{{sch}} to role BIKESHARE_{{team_name}}_READER{{env_suffix}};
+        grant usage on schema {{compagny}}{{env_suffix}}.{{sch}} to role {{compagny}}_{{team_name}}_READER{{env_suffix}};
 
         -- Accès aux objets (Existants et Futurs)
         {% for obj in object_types %}
-            grant select on all {{obj}} in schema BIKESHARE{{env_suffix}}.{{sch}} to role BIKESHARE_{{team_name}}_READER{{env_suffix}};
-            grant select on future {{obj}} in schema BIKESHARE{{env_suffix}}.{{sch}} to role BIKESHARE_{{team_name}}_READER{{env_suffix}};
+            grant select on all {{obj}} in schema {{compagny}}{{env_suffix}}.{{sch}} to role {{compagny}}_{{team_name}}_READER{{env_suffix}};
+            grant select on future {{obj}} in schema {{compagny}}{{env_suffix}}.{{sch}} to role {{compagny}}_{{team_name}}_READER{{env_suffix}};
         {% endfor %}
     {% endfor %}
 
